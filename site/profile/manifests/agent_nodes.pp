5881dfba6efcbd086826c26d516e8c695778474b
Dave Pitts <dgapitts@gmail.com>
2021-01-30 19:57:55 +0100

    fix anoter quoting syntax bug

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,12 +2,12 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node { 'db01.puppet.vm': }
   dockeragent::node { 'db02.puppet.vm': }
-  host {‘db02.puppet.vm’:
+  host {'db02.puppet.vm':
     ensure => present,
-    ip => ‘172.18.0.2’,
+    ip => '172.18.0.2',
   }
-  host {‘db01.puppet.vm’:
+  host {'db01.puppet.vm':
     ensure => present,
-    ip => ‘172.18.0.3’,
+    ip => '172.18.0.3',
   }
 }
